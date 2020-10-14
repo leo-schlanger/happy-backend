9b7c6b0c53fa28d59ad68681e9d33c0d8d11e0b3
Schlanger <leoschlanger@gmail.com>
2020-10-14 16:22:52 -0300

    update backend

--- src/views/orphanages_view.ts
+++ src/views/orphanages_view.ts
@@ -8,6 +8,8 @@ export default {
       name: orphanage.name,
       latitude: orphanage.latitude,
       longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
       opening_hours: orphanage.opening_hours,
       open_on_weekends: orphanage.open_on_weekends,
       images: imagesView.renderMany(orphanage.images),
